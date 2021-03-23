e4d6a072e0cfc94d4b5603b2a1d270b092df852f
tif-calin <culitif@tuta.io>
2021-03-22 17:19:34 -0700

    added radio buttons, validation, new story text

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import { spansArray, inputsArray, clickButton, btnSubmit } from './handlers.js';
 
 
 // initialize state
-
 const init = () => {
     for (let i = 0; i < spansArray.length; i++) {
         inputsArray[i].placeholder = spansArray[i].textContent;
